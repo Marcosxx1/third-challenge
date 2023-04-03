367705079c790034846180671d06f7b904b383d6
Marcos Alex S <marcosxx1.ma@gmail.com>
2023-04-03 15:13:11 -0400

    method updated implemented

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -89,6 +89,39 @@ class UserController {
       return res.status(500).json({ message: 'Error creating user', error });
     }
   }
+
+  async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { name, cpf, birth, email, password, cep, qualified, patio, complement, neighborhood, locality, uf } =
+      req.body;
+
+    try {
+      const userExists = await User.findById(id);
+      if (!userExists) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+
+      userExists.name = name;
+      userExists.cpf = cpf;
+      userExists.birth = birth;
+      userExists.email = email;
+      userExists.password = password;
+      userExists.cep = cep;
+      userExists.qualified = qualified;
+      userExists.patio = patio;
+      userExists.complement = complement;
+      userExists.neighborhood = neighborhood;
+      userExists.locality = locality;
+      userExists.uf = uf;
+
+      const updatedUser = await userExists.save();
+
+      return res.status(200).json(updatedUser);
+    } catch (error) {
+      return res.status(500).json({ message: 'Error updating user', error });
+    }
+  }
+
 }
 
 export default new UserController();
